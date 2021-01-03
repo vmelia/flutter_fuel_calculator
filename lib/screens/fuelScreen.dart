b71dd89f85f3699eb3ea349032457f885c2b9fc8
Vince Melia <vince@drifta.co.uk>
2021-01-03 12:47:50 +0000

    Moved widgets back into tree.

--- lib/screens/fuelScreen.dart
+++ lib/screens/fuelScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_fuel_calculator/widgets/button_widget.dart';
 import 'package:flutter_fuel_calculator/widgets/edit_string_widget.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_fuel_calculator/view_models/fuel_view_model.dart';
 
 class FuelScreen extends StatelessWidget {
@@ -14,61 +14,7 @@ class FuelScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<FuelViewModel>(context);
-
-    var distanceField = EditStringWidget(
-      controlKey: Key('distanceField'),
-      controller: distanceController,
-      onChanged: (value) => viewModel.distance = double.tryParse(value),
-      labelText: 'Distance',
-      hintText: 'E.g., 123',
-    );
-    var distancePerUnitField = EditStringWidget(
-      controlKey: Key('distancePerUnitField'),
-      controller: distancePerUnitController,
-      onChanged: (value) => viewModel.distancePerUnit = double.tryParse(value),
-      labelText: 'Distance per Unit',
-      hintText: 'E.g., 15',
-    );
-    var priceField = EditStringWidget(
-      controlKey: Key('priceField'),
-      controller: priceController,
-      onChanged: (value) => viewModel.price = double.tryParse(value),
-      labelText: 'Price',
-      hintText: 'E.g., 1.65',
-    );
-
-    var currencyDropdown = DropdownButton<String>(
-      key: Key('currencyDropdown'),
-      items: viewModel.currencies.map((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-      value: viewModel.currency,
-      onChanged: (String value) {
-        viewModel.currency = value;
-      },
-    );
-
-    var submitButton = ButtonWidget(
-        controlKey: Key('submitButton'),
-        text: 'Submit',
-        isEnabled: () => viewModel.canSubmit,
-        onPressed: () => viewModel.submit());
-
-    var resetButton = ButtonWidget(
-        controlKey: Key('resetButton'),
-        text: 'Reset',
-        isEnabled: () => viewModel.canReset,
-        onPressed: () {
-          distanceController.text = '';
-          distancePerUnitController.text = '';
-          priceController.text = '';
-          viewModel.reset();
-        });
-
+    final viewModel = Provider.of<FuelViewModel>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text("Trip Cost Calculator"),
@@ -79,32 +25,82 @@ class FuelScreen extends StatelessWidget {
         child: Column(children: <Widget>[
           Padding(
             padding: EdgeInsets.only(top: _formDistance, bottom: _formDistance),
-            child: distanceField,
+            child: EditStringWidget(
+              controlKey: Key('distanceField'),
+              controller: distanceController,
+              onChanged: (value) => viewModel.distance = double.tryParse(value),
+              labelText: 'Distance',
+              hintText: 'E.g., 123',
+            ),
           ),
           Padding(
             padding: EdgeInsets.only(top: _formDistance, bottom: _formDistance),
-            child: distancePerUnitField,
+            child: EditStringWidget(
+              controlKey: Key('distancePerUnitField'),
+              controller: distancePerUnitController,
+              onChanged: (value) =>
+                  viewModel.distancePerUnit = double.tryParse(value),
+              labelText: 'Distance per Unit',
+              hintText: 'E.g., 15',
+            ),
           ),
           Padding(
             padding: EdgeInsets.only(top: _formDistance, bottom: _formDistance),
-            child: priceField,
+            child: EditStringWidget(
+              controlKey: Key('priceField'),
+              controller: priceController,
+              onChanged: (value) => viewModel.price = double.tryParse(value),
+              labelText: 'Price',
+              hintText: 'E.g., 1.65',
+            ),
           ),
-          Text(
-            calculateResultText(viewModel.totalCost, viewModel.currency),
-            //"Total cost = ${viewModel.totalCost.toStringAsFixed(2)} $viewModel.currency",
-            key: Key('resultText'),
+          Consumer<FuelViewModel>(
+            builder: (_, viewModel, __) => Text(
+              calculateResultText(viewModel.totalCost, viewModel.currency),
+              key: Key('resultText'),
+            ),
           ),
-          currencyDropdown,
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: submitButton,
-              ),
-              VerticalDivider(width: 20),
-              Expanded(
-                child: resetButton,
-              ),
-            ],
+          DropdownButton<String>(
+            key: Key('currencyDropdown'),
+            items: viewModel.currencies.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            value: viewModel.currency,
+            onChanged: (String value) {
+              viewModel.currency = value;
+            },
+          ),
+          Consumer<FuelViewModel>(
+            builder: (_, viewModel, __) {
+              return Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ButtonWidget(
+                        controlKey: Key('submitButton'),
+                        text: 'Submit',
+                        isEnabled: () => viewModel.canSubmit,
+                        onPressed: () => viewModel.submit()),
+                  ),
+                  VerticalDivider(width: 20),
+                  Expanded(
+                    child: ButtonWidget(
+                      controlKey: Key('resetButton'),
+                      text: 'Reset',
+                      isEnabled: () => viewModel.canReset,
+                      onPressed: () {
+                        distanceController.text = '';
+                        distancePerUnitController.text = '';
+                        priceController.text = '';
+                        viewModel.reset();
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
           )
         ]),
       ),
